85ee151f3b3cedb3be15073804998370fca7dbbf
sofik <sofikoshka2002@yandex.ru>
2025-05-01 15:04:17 +0300

    module was rename to favouritebook and done

--- intentapp/src/main/java/com/mirea/semyenovasa/intentapp/SecondActivity.java
+++ intentapp/src/main/java/com/mirea/semyenovasa/intentapp/SecondActivity.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+package com.mirea.semyenovasa.intentapp;
 
-public class SecondActivity {
+import static android.content.Intent.getIntent;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+        TextView textView = findViewById(R.id.textView);
+
+        // Извлекаем сообщение из интента
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.containsKey("message")) {
+            String message = extras.getString("message");
+            textView.setText(message);
+        }
+    }
+
 }
